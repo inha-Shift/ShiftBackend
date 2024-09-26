dbfe266b2a407c62092f3ee7085791731bc32d5d
kwon <rnjsdndud00@naver.com>
2024-09-26 15:26:53 +0900

    chore: backend repository 분리

---
+++ src/test/java/com/inha/shift/ShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inha.shift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
